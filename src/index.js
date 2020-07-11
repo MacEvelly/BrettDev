d9fedfa8064db923ee734360faf7a374b57e90e1
Brett Williams <brett@brettwilliams.net>
2020-07-11 17:16:55 -0500

    react router fix?

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./setupJS/serviceWorker";
-//import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router path={process.env.PUBLIC_URL + '/'}>
       <App />
     </Router>
   </React.StrictMode>,
