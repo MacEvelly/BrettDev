afa976c2392757c10eed333324188b2027ae9806
Brett Williams <brett@brettwilliams.net>
2020-07-11 13:02:55 -0500

    Updates

---
+++ precache-manifest.198eef5c963924e7c137d5a2495611e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6872602fae81e6fafbf59f98279982f",
+    "url": "/BrettDev/index.html"
+  },
+  {
+    "revision": "4c22907c38d136618b7e",
+    "url": "/BrettDev/static/css/main.0b086690.chunk.css"
+  },
+  {
+    "revision": "06d3cb4fa5b4e3b275ee",
+    "url": "/BrettDev/static/js/2.e6fc6e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BrettDev/static/js/2.e6fc6e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c22907c38d136618b7e",
+    "url": "/BrettDev/static/js/main.a4dd45cb.chunk.js"
+  },
+  {
+    "revision": "b66b5c365cbb317cc17b",
+    "url": "/BrettDev/static/js/runtime-main.8f50a0ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BrettDev/static/media/logo.5d5d9eef.svg"
+  }
+]);
