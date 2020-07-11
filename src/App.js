8d40f018aa558bdf31b517d082a0e4dea629e697
Brett Williams <brett@brettwilliams.net>
2020-07-11 16:51:18 -0500

    testing React Router & Motion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, NavLink, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import './App.css';
 
-function App() {
-  return (
+/*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -12,7 +12,45 @@ function App() {
         </p>
       </header>
     </div>
-  );
+*/
+
+
+const MyNav = () => (
+  <ul>
+    <li><NavLink exact key={1} to={`/`}       >Home    </NavLink></li>
+    <li><NavLink exact key={1} to={`/about`}  >About   </NavLink></li>
+    <li><NavLink exact key={1} to={`/setting`}>Setting </NavLink></li>
+  </ul>
+)
+
+const pageTransition = {
+  in :{ opacity:1, y:0 },
+  out:{ opacity:0, y:"-100%"}
+}
+const myMotion = {
+  initial:"out", 
+  animate:"in", 
+  exit:"out",
+  variants:pageTransition
 }
 
+const Home    = () => (<motion.div {...myMotion} >Home: This is a home section</motion.div>)
+const Setting = () => (<motion.div {...myMotion} >Setting: This is a setting section</motion.div>)
+const About   = () => (<motion.div {...myMotion} >About: This is a about section</motion.div>)
+
+const App = () => {
+  var location = useLocation()
+  return(
+  <>
+    <MyNav />
+    <h1>Hello World</h1>
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.key}>
+        <Route path="/setting" component={Setting} />
+        <Route path="/about"   component={About}   />
+        <Route path="/"        component={Home}    />
+      </Switch>
+    </AnimatePresence>
+  </>
+)}
 export default App;
